64aba0de5d35f9b0f2b54ed3f49d21ce249cff1f
AfroStalin <chernecoff@gmail.com>
2017-01-11 16:28:52 +0300

    [FireNET] Removed CryEngine module from project
    Removed redis server from bin folder

--- src/modules/cryengine/src/Global.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright © 2016 Ilya Chernetsov. All rights reserved. Contacts: <chernecoff@gmail.com>
-// License: http://opensource.org/licenses/MIT
-
-#include <StdAfx.h>
-#include "Global.h"
-#include "../includes/FireNET_Base.h"
-
-SModuleEnv* gModuleEnv = new SModuleEnv;
--- src/modules/cryengine/src/StdAdx.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright © 2016 Ilya Chernetsov. All rights reserved. Contacts: <chernecoff@gmail.com>
-// License: http://opensource.org/licenses/MIT
-
-#include <StdAfx.h>
-#include <CryCore/Platform/platform_impl.inl>
