1dec55c0e0d6932f0f676f29fa8926a522d52332
AfroStalin <chernecoff@gmail.com>
2017-08-09 00:06:33 +0300

    [FireNET] Added script for generate solution with NO_UI option

--- GenerateSolution_Win64_VS2015_NO_UI.bat
+++ GenerateSolution_Win64_VS2015_NO_UI.bat
@@ -1,7 +1,7 @@
 @echo off
 RMDIR /s/q build
 mkdir build && cd build
-cmake .. -G "Visual Studio 14 2015 Win64"
+cmake .. -G "Visual Studio 14 2015 Win64" -DNO_UI=ON
 if ERRORLEVEL 1 (
 	pause
 ) else (
